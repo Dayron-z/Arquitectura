93ad79ce528eee36e665322e70c9ea9e079d43cf
Dayron-z <104926781+Dayron-z@users.noreply.github.com>
2024-07-28 15:40:48 -0500

    arquitectura base

--- src/main/java/com/library/library/infrastructure/abstract_services/IBookService.java
+++
@@ -1,8 +0,0 @@
-package com.library.library.infrastructure.abstract_services;
-
-import com.library.library.api.dto.request.used_request.BookRequest;
-import com.library.library.api.dto.response.used_responses.BookResponse;
-
-public interface IBookService extends CrudService <BookRequest, BookResponse, Long> {
-    public BookResponse update(BookRequest request, Long id);
-}
--- src/main/java/com/library/library/infrastructure/abstract_services/ILoanService.java
+++
@@ -1,8 +0,0 @@
-package com.library.library.infrastructure.abstract_services;
-
-import com.library.library.api.dto.request.used_request.LoanRequest;
-import com.library.library.api.dto.response.used_responses.LoanResponse;
-
-public interface ILoanService extends CrudService <LoanRequest, LoanResponse, Long> {
-     LoanResponse update(LoanRequest request, Long id);
-}
